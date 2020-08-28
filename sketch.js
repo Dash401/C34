afa5954706d0b659376f59b0a91cd87b34aed1e2
Dash <darsh.raheja4@gmail.com>
2020-08-28 18:19:26 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
-var ball;
+var ball, databas;
 
 function setup(){
+    databas = firebase.database()
+    var ballpos = databas.ref("Ball/Position");
+    ballpos.on("value",Readposition )
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
@@ -9,21 +12,29 @@ function setup(){
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    databas.ref("Ball/Position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    });
+}
+
+function Readposition(data){
+    var position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
 }
